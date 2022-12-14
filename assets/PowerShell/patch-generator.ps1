4955cfd4ef0f260d7fce0968c2383775d7651352
Erik Frantz <bardez@bardez.net>
2022-12-14 13:06:46 -0600

    Allow for index toekn in path

--- assets/PowerShell/patch-generator.ps1
+++ assets/PowerShell/patch-generator.ps1
@@ -1,15 +1,15 @@
 if ($args[0] -eq '--help')
 {
-    Write-Output 'parameter 1: JSON path'
+    Write-Output 'parameter 1: JSON path. May use token `%i` for index'
     Write-Output 'parameter 2: test value'
     Write-Output 'parameter 3: test count'
 }
 elseif ($args.count -eq 3)
 {
-    <# Action when all if and elseif conditions are false #>
-    for ( $i = $args[2] - 1; $i -gt 0; $i-- )
+    for ( $i = $args[2] - 1; $i -gt -1; $i-- )
     {
-        $patch = '  [{"op":"test","path":"' + $args[0] + '/' + $i + '","value":"' + $args[1] + '"},{"op":"remove","path":"' + $args[0] + '/' + $i + '"}],'
+        $path = $args[0] -replace '%i', $i
+        $patch = '  [{"op":"test","path":"' + $path + '","value":' + $args[1] + '},{"op":"remove","path":"' + $args[0] + '/' + $i + '"}],'
         Write-Output $patch
     }
 }
